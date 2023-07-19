d5114f61809a920b0222eb433715313279f52403
Gero <geroortizz123@gmail.com>
2023-07-19 18:16:00 -0300

    first commit - project creation + dependencies

---
+++ src/main/java/com/example/aquatrack_backend/AquaTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aquatrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaTrackBackendApplication.class, args);
+    }
+
+}

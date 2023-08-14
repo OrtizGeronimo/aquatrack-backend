58a6805cb4db9db39385c122ae2ff78024b5446f
Gero <geroortizz123@gmail.com>
2023-08-14 17:38:30 -0300

    fix jakarta problem

--- src/main/java/com/example/aquatrack_backend/model/DiaDomicilio.java
+++ src/main/java/com/example/aquatrack_backend/model/DiaDomicilio.java
@@ -1,15 +1,10 @@
 package com.example.aquatrack_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import javax.persistence.*;
 
 @Entity
 @Getter
--- src/main/java/com/example/aquatrack_backend/model/TipoPedido.java
+++ src/main/java/com/example/aquatrack_backend/model/TipoPedido.java
@@ -1,14 +1,14 @@
 package com.example.aquatrack_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity

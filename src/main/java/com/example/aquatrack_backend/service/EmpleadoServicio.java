7f8c645ce047061912a913831c7ddfb8b19862cb
Martiniano Juarroz Surballe <martijuarroz11@hotmail.com>
2023-09-05 18:01:33 -0300

    otro cambio mas

--- src/main/java/com/example/aquatrack_backend/service/EmpleadoServicio.java
+++ src/main/java/com/example/aquatrack_backend/service/EmpleadoServicio.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 import com.example.aquatrack_backend.dto.EmpleadoDTO;
 import com.example.aquatrack_backend.model.Empleado;
@@ -22,30 +23,29 @@ public class EmpleadoServicio extends ServicioBaseImpl<Empleado> {
     super(repoBase);
   }
 
-  //  public Page<EmpleadoDTO> findAllByEnterprise(int page, int size, String nombre, boolean mostrarInactivos) {
-  //     Empresa empresa = ((Empleado) getUsuarioFromContext().getPersona()).getEmpresa();
-  //     Long id = empresa.getId();
-  //     Pageable paging = PageRequest.of(page, size);
-  //     Page<Empleado> empleados = empleadoRepo.findAllByEnterprise(id, nombre, mostrarInactivos, paging);
-  //     Page<EmpleadoDTO> empleadosDTO;
-  //       for (Empleado empleado : empleados) {
-  //           EmpleadoDTO empleadoDTO = new EmpleadoDTO();
-  //           empleadoDTO.setId((Long) empleado.getId());
-  //           empleadoDTO.setNombre((String) empleado.getNombre());
-  //           empleadoDTO.setApellido((String) empleado.getApellido());
-  //           empleadoDTO.setLegajo((Integer) empleado.getLegajo());
-  //           empleadoDTO.setFechaFinVigencia((LocalDateTime) empleado.getFechaFinVigencia());
-  //           empleadoDTO.setFechaIngreso((LocalDateTime) empleado.getFechaIngreso());
-  //           empleadoDTO.setTipo((String) empleado.getTipo().getNombre());
-  //           empleadosDTO.add(empleadoDTO);
-  //       }
-    
-  // }
-
    public Page<EmpleadoDTO> findAllByEnterprise(int page, int size, String nombre, boolean mostrarInactivos) {
       Empresa empresa = ((Empleado) getUsuarioFromContext().getPersona()).getEmpresa();
       Long id = empresa.getId();
       Pageable paging = PageRequest.of(page, size);
-      return empleadoRepo.findAllByEnterprise(id, nombre, mostrarInactivos, paging).map(empleado -> new ModelMapper().map(empleado, EmpleadoDTO.class));
-   }
+      Page<Empleado> empleados = empleadoRepo.findAllByEnterprise(id, nombre, mostrarInactivos, paging);
+      Page<EmpleadoDTO> empleadosDTO = empleados.map(empleado -> {
+        EmpleadoDTO empleadoDTO = new EmpleadoDTO();
+        empleadoDTO.setId((Long) empleado.getId());
+        empleadoDTO.setNombre((String) empleado.getNombre());
+        empleadoDTO.setApellido((String) empleado.getApellido());
+        empleadoDTO.setLegajo((Integer) empleado.getLegajo());
+        empleadoDTO.setFechaFinVigencia((LocalDateTime) empleado.getFechaFinVigencia());
+        empleadoDTO.setFechaIngreso((LocalDateTime) empleado.getFechaIngreso());
+        empleadoDTO.setTipo((String) empleado.getTipo().getNombre());
+        return empleadoDTO;
+    });
+    return empleadosDTO;
+  }
+
+  //  public Page<EmpleadoDTO> findAllByEnterprise(int page, int size, String nombre, boolean mostrarInactivos) {
+  //     Empresa empresa = ((Empleado) getUsuarioFromContext().getPersona()).getEmpresa();
+  //     Long id = empresa.getId();
+  //     Pageable paging = PageRequest.of(page, size);
+  //     return empleadoRepo.findAllByEnterprise(id, nombre, mostrarInactivos, paging).map(empleado -> new ModelMapper().map(empleado, EmpleadoDTO.class));
+  //  }
 }

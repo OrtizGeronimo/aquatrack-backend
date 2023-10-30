1ce151795553a3eeffd9ef767d5af111a03278a0
MartinCarrion01 <martincarrion2864212@gmail.com>
2023-10-30 12:49:00 -0300

    endpoint name

--- src/main/java/com/example/aquatrack_backend/controller/ReporteControlador.java
+++ src/main/java/com/example/aquatrack_backend/controller/ReporteControlador.java
@@ -2,7 +2,6 @@ package com.example.aquatrack_backend.controller;
 
 import com.example.aquatrack_backend.exception.RecordNotFoundException;
 import com.example.aquatrack_backend.exception.ValidacionException;
-import com.example.aquatrack_backend.reports.ReporteReparto;
 import com.example.aquatrack_backend.service.RepartoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reports")
+@RequestMapping("/reportes")
 public class ReporteControlador {
 
     @Autowired
@@ -27,5 +26,4 @@ public class ReporteControlador {
                 .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
                 .body(repartoServicio.reporte(id));
     }
-
 }
